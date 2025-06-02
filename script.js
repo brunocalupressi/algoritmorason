37bde6ac0b82c19b0ebca452bbabe975538b623b
Bruno Calupressi <33986886+brunocalupressi@users.noreply.github.com>
2025-06-02 17:35:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var1 = Number(prompt("Ingrese la herencia"));
+carlos = var1 / 3;
+jose = (4 * carlos) / 3;
+marta = (jose / 2);
+    alert("Ah carlos le toca: " + carlos + ", a jose le toca: " + jose + " y a marta le toca: " + marta);
